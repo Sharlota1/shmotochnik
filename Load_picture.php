5b0690ce32cd5f856620857dc81b652057f8e0c7
Kristina Goloschapova <goloschapova.kristina@yandex.ua>
2016-11-12 19:42:00 +0200

    create a new form

--- Load_picture.php
+++ Load_picture.php
@@ -3,17 +3,46 @@
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>Отправка изображения на сервер</title>
+<link rel="stylesheet" type="text/css" href="upload_image.css">
 	</head>
 
 	 <body>
 	 	<form enctype="multipart/form-data" method="post">
-	 		<p>Загрузите ваши фотографии на сервер</p>
-	 		<p><input type="file" name="image" accept="image/*">
-   			<input type="submit" name="submit" value="Отправить"></p>
+	 		<div class="upload_image">
+	 			<div class="row_upload_image">
+	 				<span>Выберите файл с изображением:</span>
+			 		<input type="file" name="image" accept="image/*">
+		   		</div>
+
+		   		<div class="row_upload_image">
+	 				<span>Введите описание одежды:</span>
+			 		<textarea id="description_clother" placeholder="Description"></textarea>
+		   		</div>
+
+		   		<div class="row_upload_image">
+	 				<span>Выберите тип одежды:</span>
+	 				<div class="check_clother">
+		 				<input type="checkbox">Домашняя одежда<br />
+		 				<input type="checkbox">Парадная одежда<br />
+	 				</div>
+	 				<div class="check_clother">
+	 					<input type="checkbox">Спортивная одежда<br />
+		 				<input type="checkbox">Повседневная одежда<br />
+		 			</div>
+		   		</div>
+
+		   		<div class="row_upload_image">
+	 				<span>Температура:</span>
+				 		<input type="number" id="min_temperature" placeholder="min">
+				 		<input type="number" id="max_temperature" placeholder="max">
+		   		</div>
+
+		   	<input type="submit" name="submit" value="Отправить">
+	   		</div>
 	 	</form>
 
 	 	<?php
-	 		if(isset($_POST['submit'])) {
+	 		/*if(isset($_POST['submit'])) {
 	 			if (getimagesize($_FILES['image']['tmp_name']) == FALSE) {
 	 				echo "Пожалуйста, выберите изображение!";
 	 			} else {
@@ -35,7 +64,7 @@
 	 			} else {
 	 				echo "<br /> Ошибка! Изображение не добавлено. Повторите попытку!";
 	 			}
-	 		}
+	 		}*/
 	 	?>
 	 </body>
 </html>
