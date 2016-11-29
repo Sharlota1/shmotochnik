7c7b294e9a457f85f10f9892acd6ba7151a8bbf6
redniikaaaaa <veronika_klkv@mail.ru>
2016-11-29 22:12:28 +0200

    personal page

---
+++ dashboard.php
@@ -0,0 +1,44 @@
+<?php session_start();
+?>
+
+<html>
+	<head>
+		<title>Шмоточник</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		
+	</head>
+	<body>
+
+			<div id="wrapper">
+			
+
+					<div id="main">
+
+							<article id="me" class="panel">
+								<header>
+									<h1>Welcome</h1>
+								</header>	
+							</article>
+
+								
+							
+
+					</div>
+                   
+					<div id="footer">
+						<ul class="copyright">
+							<li>Шмоточник</li><li>Дизайн: девчата</li>
+						</ul>
+					</div>
+
+			</div>
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
